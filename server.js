753e36f1d18977f5666a7e16637986578064f9d8
graphicscycle <appgraphicscycle2@gmail.com>
2025-05-23 17:00:34 +0600

    Fix Heroku deployment issues

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const fs = require('fs');
 const cors = require('cors');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
